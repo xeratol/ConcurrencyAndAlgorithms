ef3beca4afd2be5615fd9e25ced2eb340bf693d5
Francis Joseph Serina <francis.serina@gmail.com>
2018-10-14 22:05:54 -0400

    added unity compute shaders demo

---
+++ source/Unity/Assets/SampleScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+    public ComputeShader shader;
+
+    RenderTexture _tex;
+    int _kernelHandle;
+    Renderer _renderer;
+
+    private void Awake()
+    {
+        _tex = new RenderTexture(256, 256, 24);
+        _tex.enableRandomWrite = true;
+        _tex.Create();
+
+        _kernelHandle = shader.FindKernel("CSMain");
+        shader.SetTexture(_kernelHandle, "Result", _tex);
+
+        _renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        shader.SetFloat("time", Mathf.Repeat(Time.timeSinceLevelLoad, 1.0f));
+        shader.Dispatch(_kernelHandle, 256 / 8, 256 / 8, 1);
+
+        _renderer.material.SetTexture("_MainTex", _tex);
+    }
+
+    private void OnDestroy()
+    {
+    }
+}
